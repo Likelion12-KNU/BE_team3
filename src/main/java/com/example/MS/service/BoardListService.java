6e9f90ce467131363f9f65181c955d5b3fdae394
seoyoungjin23 <ascd050@gmail.com>
2024-05-31 19:47:10 +0900

    [fix] boardlist 오류 수정

--- src/main/java/com/example/MS/service/BoardListService.java
+++ src/main/java/com/example/MS/service/BoardListService.java
@@ -1,7 +1,7 @@
 package com.example.MS.service;
 
 import com.example.MS.dto.BoardListDTO.BoardListResponse;
-import com.example.MS.model.BoardList; // BoardList 모델 클래스 import
+import com.example.MS.model.Post;
 import com.example.MS.repository.BoardListRepository; // BoardListRepository import
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,13 +20,13 @@ public class BoardListService {
     }
 
     public List<BoardListResponse> getBoardList() {
-        List<BoardList> boardList = boardListRepository.findAll(); // 게시글 목록을 DB에서 조회하는 메소드 호출
+        List<Post> boardList = boardListRepository.findAll(); // 게시글 목록을 DB에서 조회하는 메소드 호출
         return boardList.stream()
                 .map(this::mapToBoardListResponse)
                 .collect(Collectors.toList());
     }
 
-    private BoardListResponse mapToBoardListResponse(BoardList boardList) {
+    private BoardListResponse mapToBoardListResponse(Post boardList) {
         BoardListResponse response = new BoardListResponse();
         response.setId(boardList.getId());
         response.setTitle(boardList.getTitle());
